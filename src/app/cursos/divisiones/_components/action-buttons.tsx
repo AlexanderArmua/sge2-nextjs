70949cd92a543a686b27b14c89c041e2b7b21e81
Dylanhurtado1 <dhurtado@frba.utn.edu.ar>
2024-10-01 22:24:00 -0300

    Fix nueva division

--- src/app/cursos/divisiones/_components/action-buttons.tsx
+++ src/app/cursos/divisiones/_components/action-buttons.tsx
@@ -3,8 +3,8 @@ import { NuevaDivision } from "./division-new-division";
 
 export const ActionButtons = () => {
   return (
-    <>
+    <div className="relative flex w-full items-center justify-between space-y-3 md:flex-row md:justify-end">
       <NuevaDivision />
-    </>
+    </div>
   );
 };
